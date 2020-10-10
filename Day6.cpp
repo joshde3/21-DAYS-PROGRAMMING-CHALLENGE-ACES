7f480209149eb57d5d6a85e722b265b608dd686b
Joshua Deshmukh <joshdeshmukh@gmail.com>
2020-10-10 23:31:25 +0530

    Learnt impilcit conversion of variables
    The answer variable "c" was declared integer. So the outcome also was converted to integer

---
+++ Day6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float a=10.1002;
+	int b=10,c;
+	c=a+b;
+	cout<<"a :"<<a<<"\n";
+	cout<<"b :"<<b<<"\n";
+	cout<<"c :"<<c<<"\n";
+	return 0;
+}
